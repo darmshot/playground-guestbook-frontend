61366dc8d24d996e4ae7b0eb12affa821a478791
stas <darmshot@gmail.com>
2024-02-22 12:22:28 +0300

    fix: mobile layout

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div>
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4">
       <div class="flex justify-center">
-        <div class="w-4/6">
+        <div class="w-full sm:w-4/6">
           <div class="rounded border border-green-300 p-3 mb-3">
             <span class="font-bold">Superadmin</span><br>
             <span class="font-bold">Email:</span> superadmin@example.com <br>
