f659ace0d0308a4a7eb34239029f6f71ccc2a3e0
Stas <darmshot@gmail.com>
2024-02-22 12:23:44 +0300

    fix: mobile layout (#4)

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -23,16 +23,16 @@ const handleLoginRegister = () => loginRegisterModalIsOpenState.value = true
 
 <template>
   <div class="border-b pb-1">
-    <div class="container mx-auto px-2 py-2">
-      <div class="w-4/6 grid-cols-3 grid items-center mx-auto">
-        <div class="flex-grow">
+    <div class="container mx-auto px-4 py-2">
+      <div class="w-full sm:w-4/6 grid-cols-2 sm:grid-cols-3 grid items-center mx-auto">
+        <div class="flex-grow hidden sm:block">
         </div>
-        <div class="text-center">
+        <div class="sm:text-center">
           <Logo class="inline-block"/>
         </div>
         <div class="flex-grow text-right">
           <template v-if="userState?.email">
-           <span class="mr-3">{{userState?.email}}</span>
+           <div class="line-clamp-1">{{userState?.email}}</div>
             <UButton @click="handleLogout">Logout</UButton>
           </template>
           <template v-else>
